67c943a54ebcce862fa533fe54f04d2aa615a039
yrl <yannick.roussel@externes.justice.gouv.fr>
2024-04-26 16:11:30 +0200

    Sécurisation de l'application

--- src/Controller/Prejudice/BrisPorteController.php
+++ src/Controller/Prejudice/BrisPorteController.php
@@ -4,15 +4,25 @@ namespace App\Controller\Prejudice;
 
 use App\Entity\BrisPorte;
 use App\Entity\Categorie;
+use App\Entity\User;
 use App\Service\Breadcrumb\Breadcrumb;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted(User::ROLE_REQUERANT)]
 #[Route('/bris-de-porte')]
 class BrisPorteController extends AbstractController
 {
+    public function __construct(
+      private Breadcrumb $breadcrumb
+    )
+    {
+
+    }
+
     #[Route('/ajouter-un-bris-de-porte', name: 'app_bris_porte_add', methods: ['POST', 'GET'], options: ['expose' => true])]
     public function add(EntityManagerInterface $em): Response
     {
@@ -20,9 +30,11 @@ class BrisPorteController extends AbstractController
       return $this->redirectToRoute('app_bris_porte_edit',['id' => $brisPorte->getId()]);
     }
 
+    #[IsGranted('edit', subject: 'brisPorte')]
     #[Route('/declarer-un-bris-de-porte/{id}', name: 'app_bris_porte_edit', methods: ['GET'], options: ['expose' => true])]
-    public function edit(Breadcrumb $breadcrumb, BrisPorte $brisPorte): Response
+    public function edit(BrisPorte $brisPorte): Response
     {
+      $breadcrumb = $this->breadcrumb;
       $breadcrumb->add('homepage.title','app_homepage');
       $breadcrumb->add('requerant.homepage.title','app_requerant_homepage');
       $breadcrumb->add('sinistre.declare_bris_porte.title', null);
--- src/Controller/Requerant/DefaultController.php
+++ src/Controller/Requerant/DefaultController.php
@@ -3,12 +3,15 @@
 namespace App\Controller\Requerant;
 
 use App\Entity\BrisPorte;
+use App\Entity\User;
 use App\Service\Breadcrumb\Breadcrumb;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted(User::ROLE_REQUERANT)]
 class DefaultController extends AbstractController
 {
     #[Route('/requerant/accueil', name: 'app_requerant_homepage')]
