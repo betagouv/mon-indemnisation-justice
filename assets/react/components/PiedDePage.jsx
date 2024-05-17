b88137194201665b2d2ce56db1d57e34e6e14e42
yrl <yannick.roussel@externes.justice.gouv.fr>
2024-05-17 17:15:11 +0200

    VO de la page d'accueil

---
+++ assets/react/components/PiedDePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Footer } from "@codegouvfr/react-dsfr/Footer";
+
+const PiedDePage = function() {
+  return (
+    <Footer
+      accessibility="fully compliant"
+    />
+  );
+}
+
+export default PiedDePage;
