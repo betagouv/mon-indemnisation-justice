97fddfcf7c5ef6232f587795d4357e2522107938
yrl <yannick.roussel@externes.justice.gouv.fr>
2024-06-05 17:48:57 +0200

    WIP correctifs

--- src/Controller/DocumentController.php
+++ src/Controller/DocumentController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 use Vich\UploaderBundle\Handler\DownloadHandler;
 
@@ -23,7 +24,7 @@ class DocumentController extends AbstractController
 
     }
 
-    #[Route('/{id}/{type}/document-upload', name: 'app_document_upload',methods: ['POST'],options: ['expose' => true])]
+    #[Route('/document/{id}/{type}', name: 'app_document_upload',methods: ['POST'],options: ['expose' => true])]
     public function upload(LiasseDocumentaire $liasseDocumentaire, Request $request): JsonResponse
     {
       $files = $request->files->all();
@@ -47,9 +48,11 @@ class DocumentController extends AbstractController
       ]);
     }
 
-    #[Route('/{id}/document-download', name: 'app_document_download',methods: ['GET'],options: ['expose' => true])]
-    public function download(?Document $document): StreamedResponse
+    #[Route('/document/{id}/{filename}', name: 'app_document_download',methods: ['GET'],options: ['expose' => true])]
+    public function download(Request $request, ?Document $document): StreamedResponse
     {
-      return $this->downloadHandler->downloadObject($document, $fileField = 'file');
+      if($document->getFilename() === $request->get('filename'))
+        return $this->downloadHandler->downloadObject($document, $fileField = 'file');
+      throw new NotFoundHttpException('Document non trouvé');
     }
 }
