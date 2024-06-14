116964d36520da36aed573adb32036f2e6f3a920
yrl <yannick.roussel@externes.justice.gouv.fr>
2024-06-14 09:07:45 +0200

    WIP mise en conformité page de connexion

--- assets/controllers/security_login_controller.jsx
+++ assets/controllers/security_login_controller.jsx
@@ -7,6 +7,7 @@ import PiedDePage from '../react/components/PiedDePage';
 import FormulaireSimple from '../react/components/Connexion/FormulaireSimple';
 import FilAriane from '../react/components/FilAriane';
 import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
+import '../styles/authentification.css';
 
 startReactDsfr({ defaultColorScheme: "system" });
 
@@ -25,15 +26,16 @@ export default class extends Controller {
         <React.StrictMode>
           <>
             <Entete user={this.userValue} version={this.versionValue} />
-            <div className="fr-container">
-              <div className="fr-grid-row">
-                <div className="fr-col-12">
-                  <FilAriane breadcrumb={this.breadcrumbValue}/>
-                </div>
-                <div className="fr-col-6">
-                </div>
-                <div className="fr-col-6">
-                  <FormulaireSimple errorMessage={this.errorMessageValue} csrfToken={this.csrfTokenValue} lastUsername={this.lastUsernameValue} />
+            <div className="pr-authentification">
+              <div className="fr-container">
+                <FilAriane breadcrumb={this.breadcrumbValue}/>
+                <div className="fr-grid-row fr-mb-6w">
+                  <section className="pr-keyboard-hands fr-col-6">
+                    <div className="pic-keyboard-hands"></div>
+                  </section>
+                  <div className="fr-col-6">
+                    <FormulaireSimple errorMessage={this.errorMessageValue} csrfToken={this.csrfTokenValue} lastUsername={this.lastUsernameValue} />
+                  </div>
                 </div>
               </div>
             </div>
--- assets/react/components/Connexion/FormulaireSimple.jsx
+++ assets/react/components/Connexion/FormulaireSimple.jsx
@@ -9,6 +9,7 @@ import { useIsModalOpen } from "@codegouvfr/react-dsfr/Modal/useIsModalOpen";
 import { trans,
   LOGIN_BTN,
   LOGIN_EMAIL,
+  GLOBAL_INFORMATIONS_REQUIREMENT,
   SECURITY_RESET_PASSWORD_SUCCESS_TITLE,
   SECURITY_RESET_PASSWORD_SUCCESS_DESCRIPTION,
   SECURITY_RESET_PASSWORD_SUBMIT,
@@ -58,81 +59,98 @@ const FormulaireSimple = ({errorMessage,csrfToken,lastUsername}) => {
   },[]);
 
   return (
-    <form method="POST" onSubmit={handleSubmit}>
-      <Hidden name="_username" value={email} />
-      <Hidden name="_password" value={password} />
-      <Hidden name="_csrf_token" value={csrfToken} />
-      <div className="fr-grid-row">
-        <div className="fr-col-12">
-          <h1>{trans(LOGIN_H1)}</h1>
-          <p>{trans(LOGIN_CONTENT)}</p>
-        </div>
-        {errorMessage &&
-        <div className="fr-col-12">
-          <Alert
-            description={errorMessage}
-            onClose={function noRefCheck(){}}
-            severity="error"
-            title={trans(LOGIN_ERROR_TITLE)}
-          />
-        </div>
-        }
-        <div className="fr-col-12">
-          <Input
-            label={trans(LOGIN_EMAIL)}
-            nativeInputProps={{placeholder: trans(LOGIN_EMAIL_EXAMPLE), value: email, onChange: ev => setEmail(ev.target.value)}}
-          />
-        </div>
-        <div className="fr-col-12">
-          <PasswordInput
-            label={trans(LOGIN_PASSWORD)}
-            nativeInputProps={{value: password, onChange: ev => setPassword(ev.target.value)}}
-          />
-        </div>
-        <div className="fr-col-12">
-          <a href="#" onClick={() => {modal.open()}}>{trans(LOGIN_FORGOTTEN_PASSWORD)}</a>
-          <Br space={2} />
-        </div>
-        <div className="fr-col-12">
-          <Submit label={trans(LOGIN_BTN)} />
-          <Br space={2} />
-        </div>
-      </div>
-      <modal.Component title={trans(LOGIN_FORGOTTEN_PASSWORD)}>
-        {!modalEmailSent &&
+    <section className="pr-form-subscribe">
+      <form method="POST" onSubmit={handleSubmit}>
+        <Hidden name="_username" value={email} />
+        <Hidden name="_password" value={password} />
+        <Hidden name="_csrf_token" value={csrfToken} />
         <div className="fr-grid-row">
           <div className="fr-col-12">
-            <Alert
-              small
-              onClose={function noRefCheck(){}}
-              severity="info"
-              description={trans(SECURITY_RESET_PASSWORD_DESCRIPTION)}
-            />
-          </div>
-          <div className="fr-col-12">
-            <Input
-              label={trans(LOGIN_EMAIL)}
-              nativeInputProps={{placeholder: trans(LOGIN_EMAIL_EXAMPLE), value: email, onChange: ev => setEmail(ev.target.value)}}
-            />
-            <Br space={1}/>
-          </div>
-          <div className="fr-col-12">
-            <Button onClick={handleSubmitResetPassword}>
-            {trans(SECURITY_RESET_PASSWORD_SUBMIT)}
-            </Button>
+            <div className="fr-p-4w">
+              <h2>{trans(LOGIN_H1)}</h2>
+              <p className="fr-my-4w">{trans(LOGIN_CONTENT)}</p>
+              <p className="fr-my-4w">{trans(GLOBAL_INFORMATIONS_REQUIREMENT)}</p>
+              {errorMessage &&
+              <div className="fr-grid-row fr-grid-row--gutters">
+                <div className="fr-col-12">
+                  <Alert
+                    description={errorMessage}
+                    onClose={function noRefCheck(){}}
+                    severity="error"
+                    title={trans(LOGIN_ERROR_TITLE)}
+                  />
+                </div>
+              </div>
+              }
+              <div className="fr-grid-row fr-grid-row--gutters">
+                <div className="fr-col-12">
+                  <Input
+                    label={trans(LOGIN_EMAIL)}
+                    nativeInputProps={{placeholder: trans(LOGIN_EMAIL_EXAMPLE), value: email, onChange: ev => setEmail(ev.target.value)}}
+                  />
+                </div>
+              </div>
+              <div className="fr-grid-row fr-grid-row--gutters">
+                <div className="fr-col-12">
+                  <PasswordInput
+                    label={trans(LOGIN_PASSWORD)}
+                    nativeInputProps={{value: password, onChange: ev => setPassword(ev.target.value)}}
+                  />
+                </div>
+              </div>
+              <div className="fr-grid-row fr-grid-row--gutters">
+                <div className="fr-col-12">
+                  <div className="fr-my-2w">
+                    <div className="fr-input-group">
+                      <a className="fr-link" href="#" onClick={() => {modal.open()}}>{trans(LOGIN_FORGOTTEN_PASSWORD)}</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="fr-grid-row fr-grid-row--gutters">
+                <div className="fr-col-12">
+                  <Submit label={trans(LOGIN_BTN)} />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        }
-        {modalEmailSent &&
-        <Alert
-          description={trans(SECURITY_RESET_PASSWORD_SUCCESS_DESCRIPTION)}
-          onClose={() => {}}
-          severity="success"
-          title={trans(SECURITY_RESET_PASSWORD_SUCCESS_TITLE)}
-        />
-        }
-      </modal.Component>
-    </form>
+        <modal.Component title={trans(LOGIN_FORGOTTEN_PASSWORD)}>
+          {!modalEmailSent &&
+          <div className="fr-grid-row">
+            <div className="fr-col-12">
+              <Alert
+                small
+                onClose={function noRefCheck(){}}
+                severity="info"
+                description={trans(SECURITY_RESET_PASSWORD_DESCRIPTION)}
+              />
+            </div>
+            <div className="fr-col-12">
+              <Input
+                label={trans(LOGIN_EMAIL)}
+                nativeInputProps={{placeholder: trans(LOGIN_EMAIL_EXAMPLE), value: email, onChange: ev => setEmail(ev.target.value)}}
+              />
+              <Br space={1}/>
+            </div>
+            <div className="fr-col-12">
+              <Button onClick={handleSubmitResetPassword}>
+              {trans(SECURITY_RESET_PASSWORD_SUBMIT)}
+              </Button>
+            </div>
+          </div>
+          }
+          {modalEmailSent &&
+          <Alert
+            description={trans(SECURITY_RESET_PASSWORD_SUCCESS_DESCRIPTION)}
+            onClose={() => {}}
+            severity="success"
+            title={trans(SECURITY_RESET_PASSWORD_SUCCESS_TITLE)}
+          />
+          }
+        </modal.Component>
+      </form>
+    </section>
   )
 }
 
