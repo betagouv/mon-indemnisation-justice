e0925f74951a1ed154580d93a467c85c85416877
yrl <yannick.roussel@externes.justice.gouv.fr>
2024-07-02 10:08:57 +0200

    WIP mise en place du raccourci de dossier

--- src/Entity/Prejudice.php
+++ src/Entity/Prejudice.php
@@ -80,6 +80,9 @@ abstract class Prejudice implements PrejudiceInterface
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $motivationProposition = null;
 
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $raccourci = null;
+
     public function __construct()
     {
       $this->dateDeclaration = new \DateTime();
@@ -234,6 +237,18 @@ abstract class Prejudice implements PrejudiceInterface
     public function setMotivationProposition(?string $motivationProposition): static
     {
         $this->motivationProposition = $motivationProposition;
+
+        return $this;
+    }
+
+    public function getRaccourci(): ?string
+    {
+        return $this->raccourci;
+    }
+
+    public function setRaccourci(?string $raccourci): static
+    {
+        $this->raccourci = $raccourci;
 
         return $this;
     }
--- src/Service/PasswordGenerator.php
+++ src/Service/PasswordGenerator.php
@@ -3,14 +3,16 @@
 namespace App\Service;
 
 class PasswordGenerator {
-  public static function new(int $length=16): string {
+  public static function new(int $length=16,bool $withSpecialChars=true): string {
     $specialChars = ['&',':','-','$'];
 
     $bytes = random_bytes(ceil($length/2));
     $tmp =  bin2hex($bytes);
-    $randTmp = rand(0,strlen($tmp)-1);
-    $randSC = rand(0,count($specialChars)-1);
-    $tmp[$randTmp]=$specialChars[$randSC];
+    if(true === $withSpecialChars) {
+      $randTmp = rand(0,strlen($tmp)-1);
+      $randSC = rand(0,count($specialChars)-1);
+      $tmp[$randTmp]=$specialChars[$randSC];
+    }
     return $tmp;
   }
 }
