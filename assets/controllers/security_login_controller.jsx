788357b98bab7e162c9ea5c6f085aa1a2dc0a8d2
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2024-09-27 09:38:28 +0200

    Améliorer le HTML

--- assets/controllers/security_login_controller.jsx
+++ assets/controllers/security_login_controller.jsx
@@ -25,10 +25,10 @@ export default class extends Controller {
             <div className="pr-authentification">
               <div className="fr-container">
                 <div className="fr-grid-row fr-mb-6w">
-                    <section className="pr-keyboard-hands fr-col-6">
+                    <section className="pr-keyboard-hands fr-col-lg-6 fr-hidden fr-unhidden-lg">
                       <div className="pic-keyboard-hands"></div>
                     </section>
-                    <div className="fr-col-6">
+                    <div className="fr-col-12 fr-col-lg-6">
                       <FormulaireSimple errorMessage={this.errorMessageValue} csrfToken={this.csrfTokenValue} lastUsername={this.lastUsernameValue} />
                     </div>
                 </div>
