11666da4f7e428151992af59c4ec6c17a8cf6dd6
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2024-11-08 08:58:37 +0100

    Création de compte en petite-vue + correctif icônes remix manquantes

--- assets/apps/sentry.ts
+++ assets/apps/sentry.ts
@@ -9,6 +9,20 @@ if (import.meta.env?.VITE_SENTRY_DSN) {
       Sentry.feedbackIntegration({
         // Additional SDK configuration goes in here, for example:
         colorScheme: "system",
+        showEmail: false,
+        isNameRequired: true,
+        formTitle: "Faire un retour",
+        nameLabel: "Prénom",
+        namePlaceholder: "Votre prénom",
+        isRequiredLabel: "(requis)",
+        messageLabel: "Description du problème",
+        messagePlaceholder: "Coquille sur le titre \"Machin\" / Le bouton \"Valider\" ne marche pas",
+        triggerLabel: "Remonter une erreur / coquille",
+        addScreenshotButtonLabel: "Faire une capture d'écran",
+        submitButtonLabel: "Remonter",
+        cancelButtonLabel: "Annuler",
+        confirmButtonLabel: "Confirmer",
+        successMessageText: "C'est transmis à l'équipe tech!"
       }),
     ],
   });
