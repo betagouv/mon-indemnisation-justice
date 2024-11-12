c1de908dc797c18a7f3472b91a6491287d7c2777
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2024-11-12 10:55:51 +0100

    Migrer les champs d'états et date vers le nouveau modèle

--- src/Entity/EtatDossierType.php
+++ src/Entity/EtatDossierType.php
@@ -6,4 +6,33 @@ enum EtatDossierType: string
 {
     case DOSSIER_INITIE = 'DOSSIER_INITIE';
     case DOSSIER_DEPOSE = 'DOSSIER_DEPOSE';
+
+    /*
+    case RENVOI_EN_CONSTITUTION = 'RENVOI_EN_CONSTITUTION';
+    case VALIDE = 'VALIDE';
+    case REJETE = 'REJETE';
+    case SIGNATURE_VALIDEE = 'SIGNATURE_VALIDEE';
+    case SIGNATURE_REJETEE = 'SIGNATURE_REJETEE';
+    case ACCORD_OFFRE = 'ACCORD_OFFRE';
+    case REFUS_OFFRE = 'REFUS_OFFRE';
+     */
+
+    public function getLibelle(): string
+    {
+        return match ($this) {
+            self::DOSSIER_INITIE => "Demande d'indemnisation en cours de constitution",
+            self::DOSSIER_DEPOSE => "Demande d'indemnisation déposée",
+            /*
+            self::RENVOI_EN_CONSTITUTION => "Demande de pièce(s) complémentaire(s) sur la demande d'indemnisation",
+            self::VALIDE => "Demande d'indemnisation validée (en attente signature)",
+            self::REJETE => "Demande d'indemnisation rejetée (en attente signature)",
+            self::SIGNATURE_VALIDEE => "Demande d'indemnisation validée",
+            self::SIGNATURE_REJETEE => "Demande d'indemnisation rejetée",
+            self::ACCORD_OFFRE => "Proposition d'indemnisation acceptée",
+            self::REFUS_OFFRE => "Proposition d'indemnisation rejetée",
+            */
+        };
+    }
+
+
 }
