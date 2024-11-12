de591adb0340f659cbedd08c94fa7fa1db85484f
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2024-11-12 15:48:35 +0100

    Etat actuel d'un dossier et requete de décompte par état

--- src/Entity/EtatDossier.php
+++ src/Entity/EtatDossier.php
@@ -5,7 +5,6 @@ namespace App\Entity;
 use App\Repository\EtatDossierRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\Persistence\Event\LifecycleEventArgs;
 
 #[ORM\Entity(repositoryClass: EtatDossierRepository::class)]
 #[ORM\Table(name: 'dossier_etats')]
