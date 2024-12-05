780a9542c8f60d8a4c8d98ba6136710aaf9d9715
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2024-12-05 16:46:49 +0100

    Reorganiser les migrations

--- migrations/Version20241205090001.php
+++ migrations/Version20241205090001.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241108163213 extends AbstractMigration
+final class Version20241205090001 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- migrations/Version20241205090002.php
+++ migrations/Version20241205090002.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241112134623 extends AbstractMigration
+final class Version20241205090002 extends AbstractMigration
 {
     public function getDescription(): string
     {
