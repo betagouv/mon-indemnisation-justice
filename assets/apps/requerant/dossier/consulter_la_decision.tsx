0f9303d460fb3eb39ee62d51b2742c9fb8d608cf
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2025-04-01 15:46:09 +0200

    Brouillon: application react

--- assets/apps/requerant/dossier/consulter_la_decision.tsx
+++ assets/apps/requerant/dossier/consulter_la_decision.tsx
@@ -1,3 +1,4 @@
+import { DocumentType } from "@/apps/agent/dossiers/models";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { disableReactDevTools } from "@/apps/requerant/dossier/services/devtools.js";
@@ -16,9 +17,44 @@ const args = JSON.parse(document.getElementById("react-arguments").textContent);
 
 const ConsulterDecisionApp = function ConsulterDecisionApp() {
   return (
-    <>
-      <h1>Consulter la décision</h1>
-    </>
+    <div className="fr-container">
+      <div className="fr-grid-row">
+        <div className="fr-col-12">
+          <h1>Consulter la décision</h1>
+
+          <p>
+            La demande d'indemnisation que vous avez initié le jeudi 15 avril
+            2024, concernant un bris de porte survenu le dimanche 30 juillet
+            2023 au logement situé 13 rue des Oliviers 49000 ANGERS donc vous
+            êtes locataire, a été acceptée.
+          </p>
+
+          <p>
+            Le montant de l'indemnisation qui vous est proposé est de{" "}
+            <span className="fr-text--bold">1234, 56 €</span>. Pour l'accepter
+            et déclencher le versement de cette somme, il vous faut signer le
+            document et nous le retourner en cliquant sur le bouton "Accepter la
+            proposition".
+          </p>
+        </div>
+
+        <div className="fr-col-lg-3 fr-col-offset-lg-9">
+          <ul class="fr-btns-group fr-btns-group--right">
+            <li>
+              <button className="fr-btn">Accepter la proposition</button>
+            </li>
+          </ul>
+        </div>
+
+        <div className="fr-col-12">
+          <object
+            data="https://static.boutique.orange.fr/medias/pdf/divers/pdf-lettre-type-resiliation.pdf"
+            type="application/pdf"
+            style={{ width: "100%", aspectRatio: "210 / 297" }}
+          ></object>
+        </div>
+      </div>
+    </div>
   );
 };
 
