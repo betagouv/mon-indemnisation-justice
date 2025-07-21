bffab85aeb2765b592f5e44c2c25c101be3d38e8
Pierre LEMEE <pierre@pierrelemee.fr>
2025-07-21 15:41:21 +0200

    Suppression du suivi de dossier par raccourci

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,6 @@
 namespace MonIndemnisationJustice\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
