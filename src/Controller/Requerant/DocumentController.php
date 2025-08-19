4284c4bab49faf65638087dae1e2ba7c744efb9f
Pierre LEMEE <pierre@pierrelemee.fr>
2025-08-19 11:45:50 +0200

    Tentative d'utilisation de S3 simple au lieu de S3 async

--- src/Controller/Requerant/DocumentController.php
+++ src/Controller/Requerant/DocumentController.php
@@ -12,6 +12,7 @@ use MonIndemnisationJustice\Entity\BrisPorte;
 use MonIndemnisationJustice\Entity\Document;
 use MonIndemnisationJustice\Entity\DocumentType;
 use MonIndemnisationJustice\Entity\Requerant;
+use Psr\Log\LoggerInterface;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\Target;
@@ -34,6 +35,7 @@ class DocumentController extends AbstractController
         private EntityManagerInterface $em,
         #[Target('default.storage')]
         protected readonly FilesystemOperator $storage,
+        protected readonly LoggerInterface $logger,
     ) {
     }
 
@@ -64,7 +66,7 @@ class DocumentController extends AbstractController
                 $this->em->persist($dossier);
                 $this->em->flush();
             } catch (UnableToWriteFile|FilesystemException $e) {
-                throw new FileException('La sauvegarde du fichier a échoué');
+                throw new FileException("La sauvegarde du fichier a échoué: {$e->getMessage()}");
             }
         }
 
@@ -104,22 +106,27 @@ class DocumentController extends AbstractController
             if (!$this->storage->has($document->getFilename())) {
                 return new Response('', Response::HTTP_NOT_FOUND);
             }
+            /** @var $stream resource */
             $stream = $this->storage->readStream($document->getFilename());
 
             return new StreamedResponse(
                 function () use ($stream) {
-                    fpassthru($stream);
-                    exit;
+                    while (!feof($stream)) {
+                        echo fread($stream, 8192);
+                        flush();
+                    }
+
+                    fclose($stream);
                 },
                 200,
                 [
-                    'Content-Transfer-Encoding', 'binary',
                     'Content-Type' => $document->getMime() ?? 'application/octet-stream',
                     'Content-Disposition' => sprintf('%sfilename="%s"', $request->query->has('download') ? 'attachment;' : '', mb_convert_encoding($document->getOriginalFilename(), 'ISO-8859-1', 'UTF-8')),
-                    'Content-Length' => fstat($stream)['size'],
                 ]
             );
         } catch (UnableToReadFile|FilesystemException|NoSuchKeyException $e) {
+            $this->logger->warning('Fichier de pièce jointe introuvable', ['id' => $document->getId()]);
+
             return new Response('', Response::HTTP_NOT_FOUND);
         }
     }
