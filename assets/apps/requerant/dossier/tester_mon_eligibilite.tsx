bf3106f6e4bd2b2badcf1905ef03a376d749212b
Pierre LEMEE <pierre@pierrelemee.fr>
2025-09-01 14:34:07 +0200

    Améliorer le React

--- assets/apps/requerant/dossier/tester_mon_eligibilite.tsx
+++ assets/apps/requerant/dossier/tester_mon_eligibilite.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/style/requerant/dossier/test_mon_eligibilite.css";
 import { TestEligibiliteForm } from "@/apps/requerant/dossier/components/TestEligibiliteForm.tsx";
+import { Stepper } from "@codegouvfr/react-dsfr/Stepper";
 
 const args = JSON.parse(document.getElementById("react-arguments").textContent);
 
@@ -10,21 +11,14 @@ const root = ReactDOM.createRoot(document.getElementById("react-app"));
 root.render(
   <React.StrictMode>
     <div className="fr-container fr-my-3w">
-      <div className="fr-stepper">
-        <h2 className="fr-stepper__title">
-          Tester votre éligibilité
-          <span className="fr-stepper__state">Étape 1 sur 3</span>
-        </h2>
-        <div
-          className="fr-stepper__steps"
-          data-fr-current-step="1"
-          data-fr-steps="3"
-        ></div>
-        <p className="fr-stepper__details">
-          <span className="fr-text--bold">Étape suivante :</span> Création de
-          votre compte
-        </p>
-      </div>
+      <Stepper
+        currentStep={1}
+        stepCount={3}
+        title="Tester votre éligibilité"
+        nextTitle="Création de
+          votre compte"
+      />
+
       <div className="fr-col-12 fr-my-2w">
         <TestEligibiliteForm
           token={args._token}
