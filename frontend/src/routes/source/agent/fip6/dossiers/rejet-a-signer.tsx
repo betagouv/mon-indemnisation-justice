e18b91f12602a0dae38e3e880866d56b74f3e9b4
Pierre LEMEE <pierre@pierrelemee.fr>
2025-10-14 16:16:33 +0200

    Inclure la date d'opération dans le formulaire des métadonnées de l'attestation

--- frontend/src/routes/source/agent/fip6/dossiers/rejet-a-signer.tsx
+++ frontend/src/routes/source/agent/fip6/dossiers/rejet-a-signer.tsx
@@ -1,7 +1,6 @@
-import {createFileRoute} from "@tanstack/react-router";
-import {ListeDossierPropositionASigner} from "@/apps/agent/dossiers/components/ListeDossierPropositionASigner.tsx";
-import {ListeDossierRejetASigner} from "@/apps/agent/dossiers/components/ListeDossierRejetASigner.tsx";
+import { createFileRoute } from "@tanstack/react-router";
+import { ListeDossierRejetASigner } from "@/apps/agent/dossiers/components/ListeDossierRejetASigner.tsx";
 
 export const Route = createFileRoute("/agent/fip6/dossiers/rejet-a-signer")({
-    component: ListeDossierRejetASigner,
-})
+  component: ListeDossierRejetASigner,
+});
