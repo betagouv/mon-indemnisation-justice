3c1032dd6505e832dd238878f2142849129449fe
Pierre LEMEE <pierre@pierrelemee.fr>
2025-10-14 14:57:54 +0200

    En cours: ré-écrire pour simplifier

--- frontend/src/apps/agent/fip6.tsx
+++ frontend/src/apps/agent/fip6.tsx
@@ -1,59 +1,54 @@
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import React, {StrictMode} from 'react'
-import '@/apps/_init'
-import {
-    createRouter, RouterProvider,
-} from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React, { StrictMode } from "react";
+import "@/apps/_init";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 
-
-import {routeTree} from '@/routes/agent-fip6.gen.ts'
-import {container} from "@/common/services/agent";
+import { routeTree } from "@/routes/agent-fip6.gen.ts";
+import { container } from "@/common/services/agent";
 import ReactDOM from "react-dom/client";
-import {Provider} from "inversify-react";
-import {Agent} from "@/common/models";
-import {plainToInstance} from "class-transformer";
+import { Provider } from "inversify-react";
+import { AgentManagerInterface } from "@/common/services/agent/agent.ts";
+import { Agent } from "@/common/models";
 
 declare global {
-    interface Window {
-        dsfr: any;
-    }
+  interface Window {
+    dsfr: any;
+  }
 }
-
-const args: any = JSON.parse(document.getElementById('react-args')?.textContent || "") ?? {};
-
 // Création du query client Tanstack
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
+
+const moi = await container.get(AgentManagerInterface.$).moi();
 
 // Création du router Tanstack
 const router = createRouter({
-    routeTree,
-    defaultPreload: 'intent',
-    defaultStaleTime: 5000,
-    scrollRestoration: true,
-    context: {
-        agent: plainToInstance(Agent, args.agent ?? {})
-    }
-})
-
+  routeTree,
+  defaultPreload: "intent",
+  defaultStaleTime: 5000,
+  scrollRestoration: true,
+  context: {
+    agent: moi,
+  },
+});
 
 // Register things for typesafety
-declare module '@tanstack/react-router' {
-    interface Register {
-        router: typeof router
-    }
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
 }
 
-const rootElement = document.getElementById('react-app')!
+const rootElement = document.getElementById("react-app")!;
 
 if (!rootElement.innerHTML) {
-    const root = ReactDOM.createRoot(rootElement)
-    root.render(
-        <StrictMode>
-            <QueryClientProvider client={queryClient}>
-                <Provider container={container}>
-                    <RouterProvider router={router}/>
-                </Provider>
-            </QueryClientProvider>
-        </StrictMode>
-    );
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <Provider container={container}>
+          <RouterProvider router={router} />
+        </Provider>
+      </QueryClientProvider>
+    </StrictMode>,
+  );
 }
--- frontend/src/common/models/Agent.ts
+++ frontend/src/common/models/Agent.ts
@@ -1,23 +1,84 @@
 import { BaseDossier } from "@/common/models/Dossier";
 import { Redacteur } from "@/common/models/Redacteur";
-import { Transform } from "class-transformer";
+import { Expose, Transform } from "class-transformer";
 
-export enum AgentPermissionType {
-  DOSSIER = "DOSSIER",
-  REDACTEUR = "REDACTEUR",
-  GESTION_PERSONNEL = "GESTION_PERSONNEL",
-  ATTRIBUTEUR = "ATTRIBUTEUR",
-  VALIDATEUR = "VALIDATEUR",
-  LIAISON_BUDGET = "LIAISON_BUDGET",
-  BETAGOUV = "BETAGOUV",
+export type TypeRoleAgent =
+  | "ROLE_AGENT_DOSSIER"
+  | "ROLE_AGENT_REDACTEUR"
+  | "ROLE_AGENT_GESTION_PERSONNEL"
+  | "ROLE_AGENT_ATTRIBUTEUR"
+  | "ROLE_AGENT_VALIDATEUR"
+  | "ROLE_AGENT_LIAISON_BUDGET"
+  | "ROLE_AGENT_BETAGOUV"
+  | "ROLE_AGENT_FORCES_DE_L_ORDRE";
+
+export class RoleAgent {
+  static readonly DOSSIER = new RoleAgent(
+    "ROLE_AGENT_DOSSIER",
+    "Consultation des dossiers",
+    "Peut rechercher et consulter les dossiers",
+  );
+  static readonly REDACTEUR = new RoleAgent(
+    "ROLE_AGENT_REDACTEUR",
+    "Rédacteur",
+    "Instruit les dossiers d'indemnisation",
+  );
+  static readonly GESTION_PERSONNEL = new RoleAgent(
+    "ROLE_AGENT_GESTION_PERSONNEL",
+    "Gestionnaire d'agent",
+    "Définit les permissions accordées aux agents",
+  );
+  static readonly ATTRIBUTEUR = new RoleAgent(
+    "ROLE_AGENT_ATTRIBUTEUR",
+    "Attributeur",
+    "Attribue les dossiers aux rédacteurs",
+  );
+  static readonly VALIDATEUR = new RoleAgent(
+    "ROLE_AGENT_VALIDATEUR",
+    "Validateur",
+    "Valide les décisions et signe les documents",
+  );
+  static readonly LIAISON_BUDGET = new RoleAgent(
+    "ROLE_AGENT_LIAISON_BUDGET",
+    "Liaison budget",
+    "Assure la liaison avec le bureau du budget",
+  );
+  static readonly BETAGOUV = new RoleAgent(
+    "ROLE_AGENT_BETAGOUV",
+    "Membre Beta.gouv",
+    "Membre de l'équipe Mon Indemnisation Justice",
+  );
+  static readonly FORCES_DE_L_ORDRE = new RoleAgent(
+    "ROLE_AGENT_FORCES_DE_L_ORDRE",
+    "Agent des forces de l'ordre",
+    "Déclare des bris de porte",
+  );
+
+  protected constructor(
+    readonly type: TypeRoleAgent,
+    readonly libelle: string,
+    readonly description: string,
+  ) {}
+
+  static liste(): RoleAgent[] {
+    return [
+      this.DOSSIER,
+      this.REDACTEUR,
+      this.GESTION_PERSONNEL,
+      this.ATTRIBUTEUR,
+      this.VALIDATEUR,
+      this.LIAISON_BUDGET,
+      this.BETAGOUV,
+    ];
+  }
+
+  static pourType(type?: TypeRoleAgent): RoleAgent | undefined {
+    console.log(type, this.liste().find((r) => r.type === type) as RoleAgent);
+    return type
+      ? (this.liste().find((r) => r.type === type) as RoleAgent)
+      : undefined;
+  }
 }
-
-export type AgentPermission =
-  | AgentPermissionType.REDACTEUR
-  | AgentPermissionType.ATTRIBUTEUR
-  | AgentPermissionType.VALIDATEUR
-  | AgentPermissionType.LIAISON_BUDGET
-  | AgentPermissionType.BETAGOUV;
 
 export type TypeAdministration = "MJ" | "GN" | "PN";
 
@@ -61,34 +122,57 @@ export class Agent {
   public nom: string;
   public courriel: string;
 
-  @Transform(({ value }: { value: string[] }) => new Set(value))
-  protected roles: Set<AgentPermission>;
-  @Transform(({ value }: { value: TypeAdministration }) =>
-    Administration.pourType(value),
+  @Expose({ name: "roles" })
+  @Transform(({ value }: { value: string[] }) => {
+    console.log(
+      value.map((r) => r as TypeRoleAgent),
+      value.map((r) => RoleAgent.pourType(r as TypeRoleAgent)),
+      //.filter((r) => !!r),
+    );
+
+    return value
+      .map((r) => RoleAgent.pourType(r as TypeRoleAgent))
+      .filter((r) => !!r);
+  })
+  protected _roles: RoleAgent[] = [];
+  @Transform(({ value }: { value: string }) =>
+    Administration.pourType(value as TypeAdministration),
   )
   public administration: Administration;
   // TODO généraliser https://stackoverflow.com/a/61732579/4558679
   @Transform(({ value }) => new Date(value))
   public readonly datePremiereConnexion: Date;
 
+  get roles(): RoleAgent[] {
+    return this._roles;
+  }
+
+  aRole(role: RoleAgent): boolean {
+    return this._roles.includes(role);
+  }
+
+  ajouterRole(role: RoleAgent): void {
+    this._roles.push(role);
+  }
+
   public estAttributeur(): boolean {
-    return this.roles.has(AgentPermissionType.ATTRIBUTEUR);
+    return this.aRole(RoleAgent.ATTRIBUTEUR);
   }
 
   public estValidateur(): boolean {
-    return this.roles.has(AgentPermissionType.VALIDATEUR);
+    return this.aRole(RoleAgent.VALIDATEUR);
   }
 
   public estRedacteur(): boolean {
-    return this.roles.has(AgentPermissionType.REDACTEUR);
+    return this.aRole(RoleAgent.REDACTEUR);
   }
 
   public estLiaisonBudget(): boolean {
-    return this.roles.has(AgentPermissionType.LIAISON_BUDGET);
+    return this.aRole(RoleAgent.LIAISON_BUDGET);
   }
 
   public estBetagouv(): boolean {
-    return this.roles.has(AgentPermissionType.BETAGOUV);
+    return this.aRole(RoleAgent.BETAGOUV);
   }
 
   public instruit(dossier: BaseDossier): boolean {
