3428c3085edf1ffd2bcf7e9079fee53989c38b90
Pierre LEMEE <pierre@pierrelemee.fr>
2025-10-14 16:16:33 +0200

    Inclure la date d'opération dans le formulaire des métadonnées de l'attestation

--- frontend/src/routes/source/agent/fip6/dossiers/a-instruire.tsx
+++ frontend/src/routes/source/agent/fip6/dossiers/a-instruire.tsx
@@ -1,13 +1,11 @@
-import {BeforeLoadContextOptions, createFileRoute} from "@tanstack/react-router";
-import {ListeDossierATransmettre} from "@/apps/agent/dossiers/components/ListeDossierATransmettre.tsx";
-import {ListeDossierAAttribuer} from "@/apps/agent/dossiers/components/ListeDossierAAttribuer.tsx";
-import {ListeDossierAInstruire} from "@/apps/agent/dossiers/components/ListeDossierAInstruire.tsx";
-import {AgentContext} from "@/routes/contexts/AgentContext.ts";
+import { createFileRoute } from "@tanstack/react-router";
+import { ListeDossierAInstruire } from "@/apps/agent/dossiers/components/ListeDossierAInstruire.tsx";
+import { AgentContext } from "@/routes/contexts/AgentContext.ts";
 
 export const Route = createFileRoute("/agent/fip6/dossiers/a-instruire")({
-    beforeLoad: ({context}: { context: AgentContext }) => {
-        // TODO redirection sinon
-        console.log(context.agent.estRedacteur());
-    },
-    component: ListeDossierAInstruire,
-})
+  beforeLoad: ({ context }: { context: AgentContext }) => {
+    // TODO redirection sinon
+    console.log(context.agent.estRedacteur());
+  },
+  component: ListeDossierAInstruire,
+});
--- frontend/src/routes/source/agent/fip6/dossiers/a-verifier.tsx
+++ frontend/src/routes/source/agent/fip6/dossiers/a-verifier.tsx
@@ -1,9 +1,6 @@
-import {createFileRoute} from "@tanstack/react-router";
-import {ListeDossierATransmettre} from "@/apps/agent/dossiers/components/ListeDossierATransmettre.tsx";
-import {ListeDossierAAttribuer} from "@/apps/agent/dossiers/components/ListeDossierAAttribuer.tsx";
-import {ListeDossierAInstruire} from "@/apps/agent/dossiers/components/ListeDossierAInstruire.tsx";
-import {ListeDossierAVerifier} from "@/apps/agent/dossiers/components/ListeDossierAVerifier.tsx";
+import { createFileRoute } from "@tanstack/react-router";
+import { ListeDossierAVerifier } from "@/apps/agent/dossiers/components/ListeDossierAVerifier.tsx";
 
 export const Route = createFileRoute("/agent/fip6/dossiers/a-verifier")({
-    component: ListeDossierAVerifier,
-})
+  component: ListeDossierAVerifier,
+});
