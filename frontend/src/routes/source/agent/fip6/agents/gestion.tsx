84ece390d901b32d0981d1c56ae30886463f8401
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2025-10-15 09:34:11 +0200

    Utiliser un conexte router asynchrone

--- frontend/src/routes/source/agent/fip6/agents/gestion.tsx
+++ frontend/src/routes/source/agent/fip6/agents/gestion.tsx
@@ -3,9 +3,9 @@ import { ValidationAgentApp } from "@/apps/agent/gestion_agents/components";
 import { AgentContext } from "@/routes/contexts/AgentContext.ts";
 
 export const Route = createFileRoute("/agent/fip6/agents/gestion")({
-  beforeLoad: ({ context }: { context: AgentContext }) => {
+  beforeLoad: async ({ context }: { context: AgentContext }) => {
     // TODO redirection sinon
-    console.log(context.agent);
+    console.log(await context.agent);
   },
   component: ValidationAgentApp,
 });
