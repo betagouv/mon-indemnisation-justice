7dd0bd83c6ffd92d33ec13e647f1d366cbcd50d6
Pierre LEMEE <pierre@pierrelemee.fr>
2025-10-23 16:08:25 +0200

    Retravailler la commande de découpage de la déclaration d'acceptation

---
+++ backend/src/Command/DecouperDeclarationAcceptationCommand.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MonIndemnisationJustice\Command;
+
+use Doctrine\ORM\EntityManagerInterface;
+use mikehaertl\pdftk\Pdf;
+use MonIndemnisationJustice\Entity\DocumentType;
+use MonIndemnisationJustice\Repository\BrisPorteRepository;
+use MonIndemnisationJustice\Service\DocumentManager;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'mij:decouper:declaration', description: "Temporaire: découper les déclarations d'acceptation")]
+class DecouperDeclarationAcceptationCommand extends Command
+{
+    public function __construct(
+        protected readonly BrisPorteRepository $dossierRepository,
+        protected readonly EntityManagerInterface $em,
+        protected readonly DocumentManager $documentManager,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->addArgument('id', InputArgument::REQUIRED)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $id = $input->getArgument('id');
+        $dossier = $this->dossierRepository->find($id);
+
+        $output->writeln('Dossier '.$dossier->getId());
+
+        $baseDirectory = sys_get_temp_dir();
+        $baseChemin = $baseDirectory.'/dossier-'.$dossier->getId();
+
+        $cheminPiSource = $baseChemin.'-pi-source.pdf';
+        $cheminPiDestination = $baseChemin.'-pi-destination.pdf';
+        $cheminDecla = $baseChemin.'-decla.pdf';
+        file_put_contents($cheminPiSource, $this->documentManager->getContenuTexte($dossier->getCourrierDecision()));
+
+        $pdf = new Pdf($cheminPiSource);
+
+        $data = $pdf->getData();
+
+        if (false === $data) {
+            throw new \LogicException($pdf->getError());
+        }
+
+        $nbPages = intval($pdf->getData()->offsetGet('NumberOfPages'));
+
+        $output->writeln("Nb. pages pour la PI du dossier {$dossier->getId()} : {$nbPages}");
+
+        $pdf = new Pdf($cheminPiSource);
+        $result = $pdf->cat(1, $nbPages - 1)->saveAs($cheminPiDestination);
+
+        if (false === $result) {
+            throw new \LogicException($pdf->getError());
+        }
+
+        $pdf = new Pdf($cheminPiSource);
+        $result = $pdf->cat($nbPages, $nbPages)->saveAs($cheminDecla);
+
+        if (false === $result) {
+            throw new \LogicException($pdf->getError());
+        }
+
+        $this->documentManager->ajouterFichierLocal($dossier, $cheminPiDestination, DocumentType::TYPE_COURRIER_MINISTERE, false);
+        $this->documentManager->ajouterFichierLocal($dossier, $cheminDecla, DocumentType::TYPE_COURRIER_REQUERANT, false);
+
+        $output->writeln("PI et décla du dossier {$dossier->getId()} correctement séparées");
+        unlink($cheminPiSource);
+        unlink($cheminPiDestination);
+        unlink($cheminDecla);
+
+        return Command::SUCCESS;
+    }
+}
