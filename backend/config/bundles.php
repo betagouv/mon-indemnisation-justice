e2d5d19fc33dee9b52c0aa02d036371cc24a474f
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2025-11-20 16:42:13 +0100

    Correctif PC: retirer knpuniversity/oauth2-client-bundle

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -19,5 +19,4 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Pentatrion\ViteBundle\PentatrionViteBundle::class => ['all' => true],
     Acsiomatic\DeviceDetectorBundle\AcsiomaticDeviceDetectorBundle::class => ['all' => true],
-    KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
 ];
