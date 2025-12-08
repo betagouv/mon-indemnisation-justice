ff3d83e12cea501867802adf8fdc9a961d64d422
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2025-12-08 08:44:12 +0100

    Créer la page les documents et rediriger la page publique vers l'espace FDO

--- backend/src/Controller/AttestationsController.php
+++ backend/src/Controller/AttestationsController.php
@@ -5,36 +5,16 @@ declare(strict_types=1);
 namespace MonIndemnisationJustice\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/forces-de-l-ordre')]
+// TODO supprimer ce controller dès lors que la page n'est plus consultée
 class AttestationsController extends AbstractController
 {
-    protected string $assetsDirectory;
-
-    public function __construct(#[Autowire(param: 'kernel.project_dir')] string $projectDirectory)
-    {
-        $this->assetsDirectory = $projectDirectory.'/assets/';
-    }
-
     #[Route('/attestations', name: 'attestations_index')]
     public function index(): Response
     {
         return $this->render('attestations.html.twig');
-    }
-
-    #[Route('/attestations/attestation-a-remettre-en-cas-d-erreur-de-porte', name: 'attestations_attestation_a_remettre')]
-    public function attestationARemettre(): Response
-    {
-        return new BinaryFileResponse($this->assetsDirectory.'/attestations/v1/attestation-a-remettre-en-cas-d-erreur-de-porte.pdf');
-    }
-
-    #[Route('/attestations/guide-de-remise-de-l-attestation', name: 'attestations_guide_de_remise')]
-    public function guideDeRemiseDLattestation(): Response
-    {
-        return new BinaryFileResponse($this->assetsDirectory.'/attestations/v1/guide-de-remise-de-l-attestation.pdf');
     }
 }
---
+++ frontend/src/apps/agent/fdo/routes/agent/fdo/les-documents.tsx
@@ -0,0 +1,78 @@
+import Download from "@codegouvfr/react-dsfr/Download";
+import React from "react";
+import { createFileRoute } from "@tanstack/react-router";
+import Tabs from "@codegouvfr/react-dsfr/Tabs";
+import urlAttestation from "@/assets/attestations/v1/attestation-a-remettre-en-cas-d-erreur-de-porte.pdf";
+import urlGuideREmise from "@/assets/attestations/v1/guide-de-remise-de-l-attestation.pdf";
+
+export const Route = createFileRoute("/agent/fdo/les-documents")({
+  component: () => (
+    <div>
+      <h1 className="fr-my-2w">Les documents</h1>
+
+      <p>
+        Vous trouverez ci-dessous les attestations dans leur version la plus
+        récente. Ces documents sont <b>uniquement</b> destinés aux agents des
+        Forces de l'ordre.
+      </p>
+
+      <p>
+        Par ailleurs, ces documents étant susceptibles d'être mis à jour
+        régulièrement, nous vous invitons à les imprimer qu'en quantité limitée
+        et revenir sur cette page pour récupérer la dernière version du document
+        requis.
+      </p>
+
+      <Tabs
+        tabs={[
+          {
+            content: (
+              <>
+                <Download
+                  label="Attestation à remettre en cas d'erreur de porte"
+                  details={""}
+                  linkProps={{
+                    href: urlAttestation,
+                  }}
+                />
+
+                <div className="fr-grid-row fr-col-12">
+                  <object
+                    data={urlAttestation}
+                    type="application/pdf"
+                    style={{ width: "100%", aspectRatio: "210/297" }}
+                  ></object>
+                </div>
+              </>
+            ),
+            label: "Attestation à remettre",
+            isDefault: true,
+          },
+          {
+            content: (
+              <>
+                <Download
+                  label="Guide de remise de l'attestation"
+                  details={""}
+                  linkProps={{
+                    href: urlGuideREmise,
+                  }}
+                />
+
+                <div className="fr-grid-row fr-col-12">
+                  <object
+                    data={urlGuideREmise}
+                    type="application/pdf"
+                    style={{ width: "100%", aspectRatio: "210/297" }}
+                  ></object>
+                </div>
+              </>
+            ),
+            label: "Guide de remise",
+            isDefault: true,
+          },
+        ]}
+      />
+    </div>
+  ),
+});
