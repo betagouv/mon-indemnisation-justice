a15b4db5298326e48d1226da682e022112bf3709
Pierre LEMEE <pierre@pierrelemee.fr>
2025-12-09 17:08:54 +0100

    Notifier le requérant que son dossier entre en instruction

--- backend/src/Entity/EtatDossierType.php
+++ backend/src/Entity/EtatDossierType.php
@@ -7,6 +7,7 @@ use MonIndemnisationJustice\Event\Event\DossierArreteSigneEvent;
 use MonIndemnisationJustice\Event\Event\DossierAttribueEvent;
 use MonIndemnisationJustice\Event\Event\DossierClotureEvent;
 use MonIndemnisationJustice\Event\Event\DossierDeposeEvent;
+use MonIndemnisationJustice\Event\Event\DossierEnCoursInstructionEvent;
 use MonIndemnisationJustice\Event\Event\DossierIndemniseEvent;
 use MonIndemnisationJustice\Event\Event\DossierInstruitPropositionEvent;
 use MonIndemnisationJustice\Event\Event\DossierInstruitRejetEvent;
@@ -60,9 +61,13 @@ enum EtatDossierType: string
         return preg_replace('/_/', '-', strtolower($this->value));
     }
 
+    /**
+     * Créer un évènement lorsque le dossier passe à cet état.
+     */
     public function creerTransitionEvent(BrisPorte $dossier): ?DossierTransitionEvent
     {
         return match ($this) {
+            self::DOSSIER_EN_INSTRUCTION => new DossierEnCoursInstructionEvent($dossier),
             self::DOSSIER_CLOTURE => new DossierClotureEvent($dossier),
             self::DOSSIER_A_ATTRIBUER => new DossierDeposeEvent($dossier),
             self::DOSSIER_A_INSTRUIRE => new DossierAttribueEvent($dossier),
