fb3bbad22e4e0a1111307da0684514fc3b9f5542
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2025-12-10 10:46:32 +0100

    Renforcer l'étanchéité des tests end2end en jouant chaque test sur une nouvelle page incognito

--- end2end/tests/fip6/lister-dossiers-a-instruire.e2e.test.ts
+++ end2end/tests/fip6/lister-dossiers-a-instruire.e2e.test.ts
@@ -1,28 +1,29 @@
-import {test, expect} from "@playwright/test";
-import {connexionAgent, getTitre} from "../helpers";
+import { test, expect } from "@playwright/test";
+import { connexionAgent, getTitre } from "../helpers";
 
-test("lister dossier à instruire", async ({page, browser}) => {
-    // Démarrer une session incognito pour éviter les effets de bord des sessions en cookie
-    const context = await browser.newContext();
-    await context.clearCookies();
+test("lister dossier à instruire", async ({ browser }) => {
+  // Démarrer une session incognito pour éviter les effets de bord des sessions en cookie
+  const context = await browser.newContext();
+  await context.clearCookies();
+  const page = await browser.newPage();
 
-    try {
-        await connexionAgent(page, "Rédacteur");
-        await page.waitForURL("/agent/redacteur/dossiers");
+  try {
+    await connexionAgent(page, "Rédacteur");
+    await page.waitForURL("/agent/redacteur/dossiers");
 
-        await expect(getTitre(page, "Les dossiers")).toBeVisible();
+    await expect(getTitre(page, "Les dossiers")).toBeVisible();
 
-        await page.goto('/agent/fip6/dossiers/a-instruire')
+    await page.goto("/agent/fip6/dossiers/a-instruire");
 
-        await expect(getTitre(page, "Dossiers à instruire")).toBeVisible();
+    await expect(getTitre(page, "Dossiers à instruire")).toBeVisible();
 
-        // Attendre que la requête xhr soit terminée
-        await page.waitForLoadState('networkidle');
+    // Attendre que la requête xhr soit terminée
+    await page.waitForLoadState("networkidle");
 
-        await expect(getTitre(page, "2 dossiers", 'h4')).toBeVisible();
+    await expect(getTitre(page, "2 dossiers", "h4")).toBeVisible();
 
-        await expect(page.locator('.mij-dossier-liste-element')).toHaveCount(2);
-    } catch (e) {
-        await context.close();
-    }
+    await expect(page.locator(".mij-dossier-liste-element")).toHaveCount(2);
+  } catch (e) {
+    await context.close();
+  }
 });
--- end2end/tests/fip6/rechercher-un-dossier.e2e.test.ts
+++ end2end/tests/fip6/rechercher-un-dossier.e2e.test.ts
@@ -1,38 +1,38 @@
-import {test, expect} from "@playwright/test";
-import {connexionAgent} from "../helpers";
+import { test, expect } from "@playwright/test";
+import { connexionAgent } from "../helpers";
 
-test("recherche dossier", async ({page, browser}) => {
-    // Démarrer une session incognito pour éviter les effets de bord des sessions en cookie
-    const context = await browser.newContext();
-    await context.clearCookies();
+test("recherche dossier", async ({ browser }) => {
+  // Démarrer une session incognito pour éviter les effets de bord des sessions en cookie
+  const context = await browser.newContext();
+  await context.clearCookies();
+  const page = await browser.newPage();
 
-    try {
-        await connexionAgent(page, "Rédacteur");
-        await page.waitForURL("/agent/redacteur/dossiers");
+  try {
+    await connexionAgent(page, "Rédacteur");
+    await page.waitForURL("/agent/redacteur/dossiers");
 
-        await expect(page.locator("h1", {hasText: "Les dossiers"})).toBeVisible();
+    await expect(page.locator("h1", { hasText: "Les dossiers" })).toBeVisible();
 
-        const locatorCorpsTableResultats = page
-            .getByTestId("tableau-dossiers-resultats")
-            .locator("tbody");
+    const locatorCorpsTableResultats = page
+      .getByTestId("tableau-dossiers-resultats")
+      .locator("tbody");
 
-        // Changement de critère de recherche : ajouter les dossiers à finaliser
-        await page.getByText("Critères de recherche").click();
-        await page.waitForLoadState("networkidle");
+    // Changement de critère de recherche : ajouter les dossiers à finaliser
+    await page.getByText("Critères de recherche").click();
+    await page.waitForLoadState("networkidle");
 
-        await page
-            .getByLabel("Statut du dossier")
-            .selectOption(["À finaliser", "À attribuer"]);
+    await page
+      .getByLabel("Statut du dossier")
+      .selectOption(["À finaliser", "À attribuer"]);
 
-        await expect(locatorCorpsTableResultats).toBeVisible();
-        await expect(locatorCorpsTableResultats.locator("tr")).toHaveCount(2);
+    await expect(locatorCorpsTableResultats).toBeVisible();
+    await expect(locatorCorpsTableResultats.locator("tr")).toHaveCount(2);
 
-        const locatorsRequerants = await locatorCorpsTableResultats
-            .locator("tr td:nth-child(2) span:first-child")
-            .all();
-        await expect(locatorsRequerants.at(0)).toHaveText("Raquel RANDT");
-    } catch (e) {
-        await context.close();
-    }
-
+    const locatorsRequerants = await locatorCorpsTableResultats
+      .locator("tr td:nth-child(2) span:first-child")
+      .all();
+    await expect(locatorsRequerants.at(0)).toHaveText("Raquel RANDT");
+  } catch (e) {
+    await context.close();
+  }
 });
