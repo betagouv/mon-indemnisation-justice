e92bd741126d9e8ad65ab3006b7dc50ea9aeb0f3
Pierre LEMEE <pierre@pierrelemee.fr>
2025-12-16 17:17:35 +0100

    Rectifier l'exécution du script post-install

--- backend/bin/postdeploy_develop.sh
+++ backend/bin/postdeploy_develop.sh
@@ -3,7 +3,7 @@
 ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
 
 # Si INSTANCE_NUMBER n'est pas défini ou vaut 0, alors jouer la migration de base de données
-if [ -z "${INSTANCE_NUMBER}" ] || [ ${INSTANCE_NUMBER} == 0 ]; then
+if [ -z "${INSTANCE_NUMBER}" ] || [ ${INSTANCE_NUMBER} == '0' ]; then
   "${ROOT}/bin/console" doctrine:migration:migrate --no-interaction --all-or-nothing
 fi
 
