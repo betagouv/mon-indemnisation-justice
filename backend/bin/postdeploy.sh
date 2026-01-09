89b2937434844f3674e03f378082451de5c52027
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2026-01-09 10:15:02 +0100

    Assurer l'exécution du script de post install

--- backend/bin/postdeploy.sh
+++ backend/bin/postdeploy.sh
@@ -2,7 +2,9 @@
 
 ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
 
-# Si INSTANCE_NUMBER n'est pas défini ou vaut 0, alors jouer la migration de base de données
+echo "Running post deploy script on instance ${INSTANCE_NUMBER}";
+
+# Si INSTANCE_NUMBER vaut 0, alors jouer la migration de base de données
 if [ -z "${INSTANCE_NUMBER}" ] || [ ${INSTANCE_NUMBER} == '0' ]; then
   "${ROOT}/bin/console" doctrine:migration:migrate --no-interaction --all-or-nothing
 fi
