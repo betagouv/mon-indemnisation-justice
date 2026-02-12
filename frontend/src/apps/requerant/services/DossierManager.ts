5b2f54ade2e71ef5c4bbdd1cc5f775c491e1b203
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2026-02-12 10:53:27 +0100

    Simplifier la gestion des erreurs

--- frontend/src/apps/requerant/services/DossierManager.ts
+++ frontend/src/apps/requerant/services/DossierManager.ts
@@ -1,13 +1,17 @@
 import { Dossier } from "@/apps/requerant/models";
 import { ServiceIdentifier } from "inversify";
-import { plainToInstance } from "class-transformer";
+import { plainToClassFromExist, plainToInstance } from "class-transformer";
 import { DossierApercu } from "@/apps/requerant/models/Dossier.ts";
-import { EtatDossierTypeCodes } from "@/apps/requerant/models/EtatDossier.ts";
 import { dateIlYaNJours } from "@/common/services/date.ts";
 
 export interface DossierManagerInterface {
   aDossier(reference: string): Promise<boolean>;
   getDossier(reference: string): Promise<Dossier | undefined>;
+  modifierDossier(
+    reference: string,
+    modifications: Partial<Dossier>,
+  ): Promise<Dossier>;
+
   mesDemandes(): Promise<DossierApercu[]>;
 }
 
@@ -24,8 +28,6 @@ export namespace DossierManagerInterface {
 export class InMemoryDossierManager implements DossierManagerInterface {
   protected dossiers: Map<string, Dossier>;
   constructor() {
-    console.log(dateIlYaNJours(3));
-
     this.dossiers = new Map(
       [
         plainToInstance(Dossier, {
@@ -46,6 +48,29 @@ export class InMemoryDossierManager implements DossierManagerInterface {
 
   getDossier(reference: string): Promise<Dossier | undefined> {
     return Promise.resolve(this.dossiers.get(reference));
+  }
+
+  modifierDossier(
+    reference: string,
+    modifications: Partial<Dossier>,
+  ): Promise<Dossier> {
+    if (!this.dossiers.has(reference)) {
+      return Promise.reject(`Aucun dossier de référence ${reference}`);
+    }
+
+    console.dir(modifications);
+
+    this.dossiers.set(
+      reference,
+      plainToClassFromExist(
+        this.dossiers.get(reference) as Dossier,
+        modifications,
+      ),
+    );
+
+    console.log(this.dossiers.get(reference));
+
+    return Promise.resolve(this.dossiers.get(reference) as Dossier);
   }
 
   mesDemandes(): Promise<DossierApercu[]> {
