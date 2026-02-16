c6ea70a47edc66e901801df026d4e1fed13754fc
Pierre LEMEE <pierre@pierrelemee.fr>
2026-02-16 12:19:38 +0100

    Étendre et adapter la liste des types de rapport au logement

--- frontend/src/apps/requerant/routes/requerant/dossier/bris-de-porte/$reference/1-bris-porte.tsx
+++ frontend/src/apps/requerant/routes/requerant/dossier/bris-de-porte/$reference/1-bris-porte.tsx
@@ -1,4 +1,3 @@
-import { SelectionCivilite } from "@/apps/requerant/composants/SelectionCivilite";
 import { container } from "@/apps/requerant/container";
 import { Alert } from "@codegouvfr/react-dsfr/Alert";
 import {
@@ -8,34 +7,33 @@ import {
   useNavigate,
 } from "@tanstack/react-router";
 import { instanceToPlain } from "class-transformer";
-import React, { KeyboardEvent } from "react";
+import React from "react";
 import { Stepper } from "@codegouvfr/react-dsfr/Stepper";
 import { RadioButtons } from "@codegouvfr/react-dsfr/RadioButtons";
 import {
-  Civilite,
   Dossier,
-  type QualiteRequerant,
+  getListeRapportAuLogement,
+  getRapportAuLogementLibelle,
   QualiteRequerants,
+  RapportAuLogement,
 } from "@/apps/requerant/models";
 import { Select } from "@codegouvfr/react-dsfr/Select";
 import ButtonsGroup from "@codegouvfr/react-dsfr/ButtonsGroup";
 import { TitreSection } from "@/apps/requerant/composants/TitreSection.tsx";
 import { useForm } from "@tanstack/react-form";
-import { SchemaValidationEtatCivil } from "@/apps/requerant/formulaires/brisDePorte/EtatCivil.schema.ts";
 import { useInjection } from "inversify-react";
 import { DossierManagerInterface } from "@/apps/requerant/services/DossierManager.ts";
 import { Loader } from "@/common/components/Loader.tsx";
 import { ErreurResourceInconnue } from "@/apps/requerant/routeur";
-import { FormInput } from "@/apps/requerant/composants/champs/form/FormInput.tsx";
 import { Input } from "@codegouvfr/react-dsfr/Input";
+import classes from "@/apps/requerant/style/form.module.css";
+import { FormInput } from "@/apps/requerant/composants/champs/form/FormInput.tsx";
 
 const DossierInconnu = ({
   data: { titre, message },
 }: {
   data: { titre: string; message: string };
 }) => {
-  console.log({ titre, message });
-
   return (
     <div className="fr-grid-row fr-grid-row--gutters">
       <div className="fr-col-12">
@@ -48,8 +46,9 @@ const DossierInconnu = ({
 export const Route = createFileRoute(
   "/requerant/dossier/bris-de-porte/$reference/1-bris-porte",
 )({
-  component: Etape1EtatCivil,
+  component: Etape1BrisPorte,
   pendingComponent: Loader,
+  // TODO: génériser
   notFoundComponent: (props: NotFoundRouteProps) => {
     const { titre, message } = props.data as ErreurResourceInconnue;
     return (
@@ -86,7 +85,7 @@ export const Route = createFileRoute(
 });
 export default Route;
 
-function Etape1EtatCivil() {
+function Etape1BrisPorte() {
   const naviguer = useNavigate({
     from: Route.fullPath,
   });
@@ -113,7 +112,7 @@ function Etape1EtatCivil() {
     },
     onSubmit: async ({ value, formApi }) => {
       await naviguer({
-        to: "../2-bris-de-porte",
+        to: "../2-infos-requerant",
         search: {} as any,
       });
     },
@@ -122,15 +121,6 @@ function Etape1EtatCivil() {
   return (
     <>
       <h1>Déclarer un bris de porte</h1>
-
-      <section>
-        <Stepper
-          currentStep={1}
-          stepCount={3}
-          title={"Informations relatives au bris de porte"}
-          nextTitle={"Données personnelles"}
-        />
-      </section>
 
       <form
         onSubmit={async (e) => {
@@ -142,7 +132,17 @@ function Etape1EtatCivil() {
             console.error(e);
           }
         }}
+        className={classes.mijForm}
       >
+        <section>
+          <Stepper
+            currentStep={1}
+            stepCount={3}
+            title={"Informations relatives au bris de porte"}
+            nextTitle={"Données personnelles"}
+          />
+        </section>
+
         <section>
           <div className="fr-grid-row">
             <div className="fr-col-12">
@@ -160,6 +160,10 @@ function Etape1EtatCivil() {
                             checked: field.state.value == false,
                             onChange: () => {
                               field.setValue(false);
+                              field.form.setFieldValue(
+                                "rapportAuLogement",
+                                undefined,
+                              );
                             },
                           },
                         },
@@ -169,6 +173,10 @@ function Etape1EtatCivil() {
                             checked: field.state.value == true,
                             onChange: () => {
                               field.setValue(true);
+                              field.form.setFieldValue(
+                                "rapportAuLogement",
+                                undefined,
+                              );
                             },
                           },
                         },
@@ -190,19 +198,19 @@ function Etape1EtatCivil() {
                       <div className="fr-grid-row fr-grid-row--gutters">
                         <div className="fr-col-lg-6 fr-col-12">
                           <formulaire.Field
-                            name="description"
+                            name="rapportAuLogement"
                             children={(field) => {
                               return (
                                 <Select
                                   label="Vous effectuez votre demande en qualité de"
                                   nativeSelectProps={{
+                                    defaultValue: field.state.value || "",
                                     onChange: (event) => {
-                                      console.log(event.target.value);
-                                      if (
-                                        Object.keys(QualiteRequerants).includes(
-                                          event.target.value,
-                                        )
-                                      ) {
+                                      if (!!event.target.value) {
+                                        field.setValue(
+                                          event.target
+                                            .value as RapportAuLogement,
+                                        );
                                       }
                                     },
                                   }}
@@ -210,13 +218,18 @@ function Etape1EtatCivil() {
                                   <option value="" disabled hidden>
                                     Selectionnez une option
                                   </option>
-                                  {Object.entries(QualiteRequerants).map(
-                                    ([key, label]) => (
-                                      <option key={key} value={key}>
-                                        {label}
-                                      </option>
-                                    ),
-                                  )}
+                                  {getListeRapportAuLogement(
+                                    estPersonneMorale,
+                                  ).map((rapportAuLogement) => (
+                                    <option
+                                      key={rapportAuLogement}
+                                      value={rapportAuLogement}
+                                    >
+                                      {getRapportAuLogementLibelle(
+                                        rapportAuLogement,
+                                      )}
+                                    </option>
+                                  ))}
                                 </Select>
                               );
                             }}
@@ -230,7 +243,7 @@ function Etape1EtatCivil() {
                               {rapportAuLogement === "AUT" && (
                                 <div className="fr-col-lg-6 fr-col-12">
                                   <formulaire.Field
-                                    name="description"
+                                    name="descriptionRapportAuLogement"
                                     children={(field) => {
                                       return (
                                         <Input
@@ -262,50 +275,60 @@ function Etape1EtatCivil() {
           selector={(state) => [
             state.values.requerant?.estPersonneMorale,
             state.values.rapportAuLogement,
+            state.values.descriptionRapportAuLogement,
           ]}
-          children={([estPersonneMorale, rapportAuLogement]) => (
+          children={([
+            estPersonneMorale,
+            rapportAuLogement,
+            descriptionRapportAuLogement,
+          ]) => (
             <>
               {estPersonneMorale !== undefined &&
-                rapportAuLogement !== undefined && (
+                rapportAuLogement !== undefined &&
+                (rapportAuLogement !== "AUT" ||
+                  !!descriptionRapportAuLogement) && (
                   <section>
                     <TitreSection>
                       Informations sur le bris de porte
                     </TitreSection>
 
                     <div className="fr-grid-row fr-grid-row--gutters">
-                      <div className="fr-col-12">
+                      <div className="fr-col-lg-4 fr-col-12">
                         <formulaire.Field
-                          name="description"
+                          name="dateOperation"
                           children={(field) => {
                             return (
-                              <Input
-                                label="Décrivez-nous l’intervention"
-                                textArea
-                                nativeTextAreaProps={{
-                                  placeholder:
-                                    "Intervention survenue ce matin ...",
-                                  onChange: (e) => console.log(e.target.value),
-                                  rows: 10,
-                                  cols: 50,
+                              <FormInput
+                                label="Date du bris de porte"
+                                nativeInputProps={{
+                                  type: "date",
+                                  onChange: (e) =>
+                                    field.setValue(new Date(e.target.value)),
                                 }}
+                                champ={field}
                               />
                             );
                           }}
                         />
                       </div>
-                    </div>
-                    <div className="fr-grid-row fr-grid-row--gutters">
-                      <div className="fr-col-lg-4 fr-col-offset-8 fr-col-12">
+
+                      <div className="fr-col-lg-8 fr-col-12">
                         <formulaire.Field
                           name="description"
                           children={(field) => {
                             return (
-                              <Input
-                                label="Date de l'opération de police judiciaire"
-                                nativeInputProps={{
-                                  type: "date",
-                                  onChange: (e) => console.log(e.target.value),
+                              <FormInput
+                                label="Décrivez-nous l’intervention"
+                                textArea
+                                nativeTextAreaProps={{
+                                  placeholder:
+                                    "Intervention survenue ce matin ...",
+                                  rows: 10,
+                                  cols: 50,
+                                  onChange: (e) =>
+                                    field.setValue(e.target.value),
                                 }}
+                                champ={field}
                               />
                             );
                           }}
@@ -313,77 +336,78 @@ function Etape1EtatCivil() {
                       </div>
                     </div>
 
-                    <div className="fr-col-12">
-                      <div className="fr-grid-row fr-grid-row--gutters">
-                        <div className="fr-col-6">
-                          <formulaire.Field
-                            name="description"
-                            children={(field) => {
-                              return (
-                                <Input
-                                  label="Adresse du logement concerné par le bris de porte"
-                                  nativeInputProps={{
-                                    onChange: (e) =>
-                                      console.log(e.target.value),
-                                    maxLength: 255,
-                                  }}
-                                />
-                              );
-                            }}
-                          />
-                        </div>
-                        <div className="fr-col-12">
-                          <formulaire.Field
-                            name="description"
-                            children={(field) => {
-                              return (
-                                <Input
-                                  label="Complément d'adresse"
-                                  hintText={"Facultatif"}
-                                  nativeInputProps={{
-                                    onChange: (e) =>
-                                      console.log(e.target.value),
-                                    maxLength: 255,
-                                  }}
-                                />
-                              );
-                            }}
-                          />
-                        </div>
-                        <div className="fr-col-lg-2 fr-col-4">
-                          <formulaire.Field
-                            name="description"
-                            children={(field) => {
-                              return (
-                                <Input
-                                  label="Code postal"
-                                  nativeInputProps={{
-                                    onChange: (e) =>
-                                      console.log(e.target.value),
-                                    maxLength: 5,
-                                  }}
-                                />
-                              );
-                            }}
-                          />
-                        </div>
-                        <div className="fr-col-lg-10 fr-col-8">
-                          <formulaire.Field
-                            name="description"
-                            children={(field) => {
-                              return (
-                                <Input
-                                  label="Ville"
-                                  nativeInputProps={{
-                                    onChange: (e) =>
-                                      console.log(e.target.value),
-                                    maxLength: 255,
-                                  }}
-                                />
-                              );
-                            }}
-                          />
-                        </div>
+                    <div className="fr-grid-row fr-grid-row--gutters">
+                      <div className="fr-col-12 fr-col-lg-6">
+                        <formulaire.Field
+                          name="requerant.adresse.ligne1"
+                          children={(field) => {
+                            return (
+                              <FormInput
+                                label="Adresse du logement concerné par le bris de porte"
+                                nativeInputProps={{
+                                  maxLength: 255,
+                                  onChange: (e) =>
+                                    field.setValue(e.target.value),
+                                }}
+                                champ={field}
+                              />
+                            );
+                          }}
+                        />
+                      </div>
+                      <div className="fr-col-12 fr-col-lg-6">
+                        <formulaire.Field
+                          name="requerant.adresse.ligne1"
+                          children={(field) => {
+                            return (
+                              <FormInput
+                                label="Complément d'adresse"
+                                nativeInputProps={{
+                                  maxLength: 255,
+                                  onChange: (e) =>
+                                    field.setValue(e.target.value),
+                                }}
+                                champ={field}
+                              />
+                            );
+                          }}
+                        />
+                      </div>
+                      <div className="fr-col-lg-2 fr-col-4">
+                        <formulaire.Field
+                          name="requerant.adresse.commune.codePostal"
+                          children={(field) => {
+                            return (
+                              <FormInput
+                                label="Code postal"
+                                nativeInputProps={{
+                                  onChange: (e) =>
+                                    field.setValue(e.target.value),
+                                  maxLength: 5,
+                                }}
+                                champ={field}
+                              />
+                            );
+                          }}
+                        />
+                      </div>
+                      <div className="fr-col-lg-10 fr-col-8">
+                        <formulaire.Field
+                          name="requerant.adresse.commune.nom"
+                          children={(field) => {
+                            return (
+                              <FormInput
+                                label="Ville"
+                                nativeInputProps={{
+                                  onChange: (e) =>
+                                    field.setValue(e.target.value),
+                                  maxLength: 255,
+                                }}
+                                champ={field}
+                              />
+                            );
+                          }}
+                        />
                       </div>
                     </div>
                     <div className="fr-col-12">
@@ -399,7 +423,7 @@ function Etape1EtatCivil() {
                                   label: "Oui",
                                   nativeInputProps: {
                                     onChange: (e) =>
-                                      console.log(e.target.checked),
+                                      field.setValue(e.target.value),
                                   },
                                 },
                                 {
@@ -407,7 +431,7 @@ function Etape1EtatCivil() {
                                   nativeInputProps: {
                                     checked: true,
                                     onChange: (e) =>
-                                      console.log(e.target.checked),
+                                      field.setValue(e.target.value),
                                   },
                                 },
                               ]}
