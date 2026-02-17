4f9b00280cc607eacf4a6b079db71d94f1926f07
Pierre LEMEE <pierre@pierrelemee.fr>
2026-02-17 18:12:14 +0100

    Réorganiser la route API qui liste les communes par code postal et le service qui y fait appel

---
+++ backend/src/Api/Requerant/Commune/ListerCommuneEndpoint.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MonIndemnisationJustice\Api\Requerant\Commune;
+
+use MonIndemnisationJustice\Entity\GeoCodePostal;
+use MonIndemnisationJustice\Entity\Requerant;
+use MonIndemnisationJustice\Repository\GeoCodePostalRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/api/requerant/communes/{codePostal}', name: 'api_requerant_commune_lister', methods: ['GET'])]
+#[IsGranted(Requerant::ROLE_REQUERANT,
+    message: "La recherche de communes par code postal est réservée aux usagers identifiés sur la plateforme",
+    statusCode: Response::HTTP_FORBIDDEN
+)]
+class ListerCommuneEndpoint
+{
+    public function __construct(
+        protected readonly GeoCodePostalRepository $geoCodePostalRepository,
+    )
+    {
+    }
+
+    public function __invoke(string $codePostal): JsonResponse
+    {
+        return new JsonResponse(array_map(
+            fn(GeoCodePostal $geoCodePostal) => [
+                'id' => $geoCodePostal->getId(),
+                'nom' => $geoCodePostal->getCommune()->getNom(),
+            ],
+            $this->geoCodePostalRepository->findBy(['codePostal' => $codePostal])
+        ));
+    }
+
+}
