ba89a2300a9d783a4f946ecc7b549a5364cdb920
Pierre LEMEE <pierre@pierrelemee.fr>
2026-02-17 16:04:26 +0100

    Rafraichir la liste des communes en fonction du code postal

---
+++ frontend/src/apps/requerant/composants/champs/RegExpInput.tsx
@@ -0,0 +1,60 @@
+import {
+  FormInput,
+  FormInputProps,
+} from "@/apps/requerant/composants/champs/form/FormInput.tsx";
+import React, { KeyboardEvent } from "react";
+
+export type RegExpInputProps = FormInputProps & {
+  motif: string;
+  empecherLaSelection?: boolean;
+  // Fonctions de nettoyage
+  nettoyage: {
+    // Nettoyage de la valeur du champ après saisie
+    onSaisie?: (valeur: string) => string;
+    // Nettoyage de la valeur avant analyse
+    onModifie?: (valeur: string) => string;
+  };
+};
+export const RegExpInput = ({
+  motif,
+  empecherLaSelection = false,
+  nativeInputProps,
+  ...props
+}: RegExpInputProps) => {
+  return (
+    <>
+      <FormInput
+        {...props}
+        nativeInputProps={{
+          ...nativeInputProps,
+          pattern: motif,
+          onFocus: empecherLaSelection
+            ? (e) => {
+                // Mettre le curseur à la fin
+                e.target.setSelectionRange(
+                  e.target.value.length + 1,
+                  e.target.value.length + 1,
+                );
+              }
+            : undefined,
+          onKeyDown: (e: KeyboardEvent<HTMLInputElement>) => {
+            if (e.key !== "Backspace") {
+              const target: HTMLInputElement = e.target as HTMLInputElement;
+
+              // Projection de la valeur du champ après la modification
+              const valeurProjetee =
+                target.value.substring(0, target.selectionStart || 0) +
+                e.key +
+                target.value.substring(target.selectionEnd || 0);
+
+              if (!valeurProjetee.match(new RegExp(target.pattern))) {
+                e.stopPropagation();
+                e.preventDefault();
+              }
+            }
+          },
+        }}
+      />
+    </>
+  );
+};
