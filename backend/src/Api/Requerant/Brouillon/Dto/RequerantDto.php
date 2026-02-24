4dfbb44dc02e4ee88b2d1220896d6541b844cd3e
Pierre LEMEE <pierre@pierrelemee.fr>
2026-02-24 16:58:14 +0100

    Créer l'entité Brouillon

---
+++ backend/src/Api/Requerant/Brouillon/Dto/RequerantDto.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MonIndemnisationJustice\Api\Requerant\Brouillon\Dto;
+
+use MonIndemnisationJustice\Entity\Civilite;
+
+class RequerantDto
+{
+    public ?bool $estPersonneMorale;
+    // public ?TypePersonneMorale $typePersonneMorale;
+    public ?string $raisonSociale;
+    public ?string $siren;
+    /*
+    public bool $estRepresentantLegal;
+    public Civilite $civiliteRepresentantLegal;
+    public string $nomRepresentantLegal?: ;
+    public string $nomNaissanceRepresentantLegal?: ;
+    public string $prenomRepresentantLegal?: ;
+    public string $courrielRepresentantLegal?: ;
+    public string $telephoneRepresentantLegal?: ;
+    */
+    public Civilite $civilite;
+    public string $nom;
+    public string $nomNaissance;
+    public string $prenom;
+    public string $courriel;
+    public string $telephone;
+    public ?AdresseDto $adresse;
+    public ?\DateTimeImmutable $dateNaissance;
+    public ?PaysDto $paysNaissance;
+}
