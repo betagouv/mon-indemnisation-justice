4051453a1414a131fff18d684a66d37307bf601d
Pierre LEMEE <pierre@pierrelemee.fr>
2026-02-27 07:52:41 +0100

    En cours : définir le schéma de données idéal

---
+++ frontend/src/apps/requerant/models/PersonneMorale.ts
@@ -0,0 +1,13 @@
+import { Personne } from "@/apps/requerant/models/Personne.ts";
+import { Adresse } from "./Adresse.ts";
+import { type TypePersonneMoraleType } from "./TypePersonneMoraleType.ts";
+
+export class PersonneMorale {
+  raisonSociale: string;
+  siren: string;
+  typePersonneMorale: TypePersonneMoraleType;
+  representantLegal: Personne;
+  delegataires?: Personne[];
+  gestionnaires?: Personne[];
+  adresse: Adresse;
+}
