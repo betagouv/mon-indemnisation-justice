6e500d00ee446a5f14602b43065c7d953c88e102
Pierre LEMEE <pierre@pierrelemee.fr>
2026-03-03 17:14:37 +0100

    Commencer à utiliser les vraies données

---
+++ backend/src/Api/Requerant/Brouillon/MesDemandesEndpoint.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MonIndemnisationJustice\Api\Requerant\Brouillon;
+
+use MonIndemnisationJustice\Entity\Brouillon;
+use MonIndemnisationJustice\Entity\BrouillonType;
+use MonIndemnisationJustice\Entity\Usager;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+#[Route('/api/requerant/mes-demandes', name: 'api_requerant_mes_demandes', methods: ['GET'])]
+class MesDemandesEndpoint
+{
+    public function __construct(private readonly NormalizerInterface $normalizer)
+    {
+    }
+
+    public function __invoke(Security $security, NormalizerInterface $normalizer)
+    {
+        /** @var Usager $usager */
+        $usager = $security->getUser();
+
+        if (!$usager instanceof Usager) {
+            throw new UnauthorizedHttpException("La liste des demandes d'indemnisation est réservée aux requérants");
+        }
+
+        return new JsonResponse(
+            $this->normalizer->normalize(
+                $usager->getBrouillons()->filter(fn (Brouillon $brouillon) => BrouillonType::BROUILLON_DOSSIER_BRIS_PORTE === $brouillon->getType())
+            ),
+            Response::HTTP_OK
+        );
+    }
+}
--- backend/src/Event/Listener/ConnexionUsagerListener.php
+++ backend/src/Event/Listener/ConnexionUsagerListener.php
@@ -3,8 +3,6 @@
 namespace MonIndemnisationJustice\Event\Listener;
 
 use Doctrine\ORM\EntityManagerInterface;
-use MonIndemnisationJustice\Api\Requerant\Brouillon\Dto\DossierDto;
-use MonIndemnisationJustice\Api\Requerant\Brouillon\Dto\UsagerDto;
 use MonIndemnisationJustice\Controller\BrisPorteController as PublicBrisPorteController;
 use MonIndemnisationJustice\Entity\BrouillonType;
 use MonIndemnisationJustice\Entity\DeclarationFDOBrisPorte;
@@ -16,7 +14,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
-class ConnexionRequerantListener implements EventSubscriberInterface
+class ConnexionUsagerListener implements EventSubscriberInterface
 {
     public function __construct(
         protected readonly EntityManagerInterface $em,
@@ -27,64 +25,62 @@ class ConnexionRequerantListener implements EventSubscriberInterface
 
     public function onSecurityInteractiveLogin(LoginSuccessEvent $event): void
     {
-        $requerant = $event->getUser();
+        $usager = $event->getUser();
 
-        if (!$requerant instanceof Usager) {
+        if (!$usager instanceof Usager) {
             return;
         }
 
         $request = $event->getRequest();
 
         // S'il existe un test d'éligibilité dans la session du requérant...
-        if (null !== ($testEligibilite = $this->getTestEligibiliteEnCours($request, $requerant))) {
+        if (null !== ($testEligibilite = $this->getTestEligibiliteEnCours($request, $usager))) {
             // ... associée à aucun de ses dossiers existants...
             if (null !== $testEligibilite && null === $testEligibilite->dossier) {
                 // ... alors, on initie un nouveau brouillon de dossier lié à ce test d'éligibilité
-                $dossier = new DossierDto();
-                $dossier->requerant = new UsagerDto();
-                $dossier->requerant->estPersonneMorale = $requerant->getIsPersonneMorale();
-                $dossier->requerant->raisonSociale = $requerant->getPersonneMorale()?->getRaisonSociale();
-                $dossier->requerant->siren = $requerant->getPersonneMorale()?->getSirenSiret();
-                // TODO finir de transvaser les champs
-                $dossier->testEligibilite = new TestEligibilite();
-                $dossier->testEligibilite->id = $testEligibilite->id;
-
-                $this->gestionnaireBrouillon->initierDepuis($dossier, $requerant);
-
-                $requerant->setNavigation(null);
-            }
-        } else {
-            // Sinon si une déclaration des FDO existe en session...
-            $declarationFDO = $this->getDeclarationFDOEnCours($request, $requerant);
-            // ... associée à aucun de ses dossiers existants...
-            if (null !== $declarationFDO && null === $declarationFDO->getDossier()) {
-                // ... alors, on crée un nouveau dossier lié à cette déclaration
-                /*
-                $dossier = new BrisPorte()
-                    ->setRequerant($requerant)
-                    ->setDeclarationFDO($declarationFDO)
-                    ->setDateOperationPJ($declarationFDO->getDateOperation())
-                    // On recrée une nouvelle adresse pour conserver les données des FDO et pouvoir plus tard comparer et arbitrer
-                    ->setAdresse(
-                        new Adresse()
-                            ->setLigne1($declarationFDO->getAdresse()->getLigne1())
-                            ->setLigne2($declarationFDO->getAdresse()->getLigne2())
-                            ->setCodePostal($declarationFDO->getAdresse()->getCodePostal())
-                            ->setLocalite($declarationFDO->getAdresse()->getLocalite())
-                    );
-                */
-
-                $this->gestionnaireBrouillon->initier(BrouillonType::BROUILLON_DOSSIER_BRIS_PORTE, requerant: $requerant, donnees: [
+                $this->gestionnaireBrouillon->initier(BrouillonType::BROUILLON_DOSSIER_BRIS_PORTE, $usager, donnees: [
+                    'idTestEligibilite' => $testEligibilite->id,
                     'requerant' => [
-                        'estPersonneMorale' => $requerant->getIsPersonneMorale(),
-                        'raisonSociale' => $requerant->getPersonneMorale()?->getRaisonSociale(),
-                        'siren' => $requerant->getPersonneMorale()?->getSirenSiret(),
-
-                        // TODO finir de transvaser les champs
+                        'personne' => [
+                            'id' => $usager->getPersonne()->getId(),
+                            'civilite' => $usager->getPersonne()->getCivilite(),
+                            'nom' => $usager->getPersonne()->getNom(),
+                            'prenom' => $usager->getPersonne()->getPrenom(),
+                            'courriel' => $usager->getPersonne()->getCourriel(),
+                            'telephone' => $usager->getPersonne()->getTelephone(),
+                        ],
                     ],
                 ]);
 
-                $requerant->setNavigation(null);
+                $usager->setNavigation(null);
+            }
+        } else {
+            // Sinon si une déclaration des FDO existe en session...
+            $declarationFDO = $this->getDeclarationFDOEnCours($request, $usager);
+            // ... associée à aucun de ses dossiers existants...
+            if (null !== $declarationFDO && null === $declarationFDO->getDossier()) {
+                // ... alors, on crée un nouveau dossier lié à cette déclaration
+                $this->gestionnaireBrouillon->initier(BrouillonType::BROUILLON_DOSSIER_BRIS_PORTE, $usager, donnees: [
+                    'idDeclarationFDO' => $declarationFDO->getId(),
+                    'dateOperation' => $declarationFDO->getDateOperation(),
+                    'adresse' => [
+                        'ligne1' => $declarationFDO->getAdresse()->getLigne1(),
+                        'ligne2' => $declarationFDO->getAdresse()->getLigne2(),
+                        'codePostal' => $declarationFDO->getAdresse()->getCodePostal(),
+                        'commune' => $declarationFDO->getAdresse()->getLocalite(),
+                    ],
+                    'requerant' => $declarationFDO->getCoordonneesRequerant() ? [
+                        'personne' => [
+                            'civilite' => $declarationFDO->getCoordonneesRequerant()?->getCivilite(),
+                            'nom' => $declarationFDO->getCoordonneesRequerant()?->getNom(),
+                            'prenom' => $declarationFDO->getCoordonneesRequerant()?->getPrenom(),
+                            'courriel' => $declarationFDO->getCoordonneesRequerant()?->getCourriel(),
+                            'telephone' => $declarationFDO->getCoordonneesRequerant()?->getTelephone(),
+                        ],
+                    ] : null,
+                ]);
+
+                $usager->setNavigation(null);
             }
         }
 
