88e589f578a5dc3aa30d113158295825ad6e88eb
Pierre LEMEE <pierre@pierrelemee.fr>
2026-03-04 17:15:29 +0100

    En cours : convertir les dossiers à finaliser en brouillons

--- frontend/src/apps/requerant/services/UsagerManager.ts
+++ frontend/src/apps/requerant/services/UsagerManager.ts
@@ -1,4 +1,6 @@
+import { Usager } from "@/apps/requerant/models";
 import { ContexteUsager } from "@/apps/requerant/routeur/contexte.ts";
+import { plainToInstance } from "class-transformer";
 import { ServiceIdentifier } from "inversify";
 
 export interface UsagerManagerInterface {
@@ -12,16 +14,19 @@ export namespace UsagerManagerInterface {
 }
 
 export class APIUsagerManager implements UsagerManagerInterface {
-  moi(): Promise<ContexteUsager> {
-    return new Promise((resolve) =>
-      resolve({
-        usager: {
-          id: 1234,
-          prenom: "Rick",
-          nom: "ERRANT",
-          courriel: "rick@courriel.fr",
-        },
-      }),
-    );
+  async moi(): Promise<ContexteUsager> {
+    const reponse = await fetch("/api/requerant/moi");
+
+    const data = await reponse.json();
+
+    console.dir({
+      incarnePar: data,
+      usager: plainToInstance(Usager, data.usager),
+    });
+
+    return {
+      incarnePar: data,
+      usager: plainToInstance(Usager, data.usager),
+    };
   }
 }
