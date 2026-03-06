6873e477267b3dcc104de2d39d413ae4914d12f6
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2026-03-06 15:36:29 +0100

    Ne patcher que les éléments réellement modifiés

--- frontend/src/common/services/index.ts
+++ frontend/src/common/services/index.ts
@@ -4,6 +4,8 @@ import {
 } from "@/common/services/RequerantManager";
 import { Container } from "inversify";
 
+export { differentiel, fusion } from "./object";
+
 const container: Container = new Container();
 container.bind(RequerantManagerImpl).to(RequerantAPICLient).inSingletonScope();
 
