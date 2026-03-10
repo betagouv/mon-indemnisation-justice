3892b9eb5055e45df56fc165334429328d3fae52
Pierre LEMEE <pierre@pierrelemee.fr>
2026-03-10 17:25:04 +0100

    Et si on faisait sans brouillon ?

--- backend/src/Api/Requerant/Brouillon/MesDemandesEndpoint.php
+++ backend/src/Api/Requerant/Brouillon/MesDemandesEndpoint.php
@@ -2,14 +2,14 @@
 
 namespace MonIndemnisationJustice\Api\Requerant\Brouillon;
 
-use MonIndemnisationJustice\Entity\Brouillon;
-use MonIndemnisationJustice\Entity\BrouillonType;
+use MonIndemnisationJustice\Api\Requerant\Brouillon\Dto\DossierDto;
+use MonIndemnisationJustice\Entity\Dossier;
 use MonIndemnisationJustice\Entity\Usager;
-use MonIndemnisationJustice\Service\GestionnaireBrouillon;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Symfony\Component\ObjectMapper\ObjectMapperInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
@@ -18,7 +18,8 @@ class MesDemandesEndpoint
 {
     public function __construct(
         protected readonly NormalizerInterface $normalizer,
-        protected readonly GestionnaireBrouillon $gestionnaireBrouillon,
+        // protected readonly DossierDtoMapper $mapper,
+        protected readonly ObjectMapperInterface $mapper,
     ) {
     }
 
@@ -33,9 +34,9 @@ class MesDemandesEndpoint
 
         return new JsonResponse(
             $this->normalizer->normalize(
-                $usager->getBrouillons()
-                    ->filter(fn (Brouillon $brouillon) => BrouillonType::BROUILLON_DOSSIER_BRIS_PORTE === $brouillon->getType())
-                    ->map(fn (Brouillon $brouillon) => $this->gestionnaireBrouillon->extraireEntiteTravail($brouillon))
+                $usager->getDossiersBrisDePorte()->map(
+                    fn (Dossier $dossier) => $this->mapper->map($dossier, DossierDto::class)
+                )->toArray(),
             ),
             Response::HTTP_OK
         );
