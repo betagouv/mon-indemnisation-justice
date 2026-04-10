ea3b7494c5a6e0091b138b570367bd3c6bd06a62
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2026-04-10 16:08:26 +0200

    Adapter le test end2end de dépôt de dossier

--- frontend/src/apps/requerant/models/TypePersonneMoraleType.ts
+++ frontend/src/apps/requerant/models/TypePersonneMoraleType.ts
@@ -26,8 +26,33 @@ const PersonneMoraleTypeLibelles: { [key in TypePersonneMoraleType]: string } =
 const getLibelleTypePersonneMorale = (type: TypePersonneMoraleType): string =>
   PersonneMoraleTypeLibelles[type];
 
+const libellerNomTypePersonneMorale = (
+  type: TypePersonneMoraleType,
+  options: { defini: boolean },
+): string => {
+  switch (type) {
+    case "ASSOCIATION":
+      return `${defini ? "l'" : "une "}association`;
+    case "ASSUREUR":
+      return `${defini ? "l'" : "un "}assureur`;
+    case "BAILLEUR_SOCIAL":
+      return `${defini ? "le " : "un "}bailleur social`;
+    case "COLLECTIVITE":
+      return `${defini ? "la " : "une "}collectivité territoriale`;
+    case "ENTREPRISE_PRIVEE":
+      return `${defini ? "l'" : "une "}entreprise du secteur privé`;
+    case "ETABLISSEMENT_PUBLIC":
+      return `${defini ? "l'" : "un "}établissement public`;
+    case "SCI":
+      return `${defini ? "la " : "une "}SCI`;
+    case "SYNDIC":
+      return `${defini ? "le " : "un "}syndic`;
+  }
+};
+
 export {
   TypesPersonneMorale,
   getLibelleTypePersonneMorale,
+  libellerNomTypePersonneMorale,
   type TypePersonneMoraleType,
 };
