5e6515d53ada30f8091bf4151cd8a815e3aa2d47
Pierre LEMEE <pierre@pierrelemee.fr>
2026-04-30 17:10:55 +0200

    Monter les versions de Symfony et Doctrine pour passage à 8.0

--- backend/src/Security/UsagerChecker.php
+++ backend/src/Security/UsagerChecker.php
@@ -3,6 +3,7 @@
 namespace MonIndemnisationJustice\Security;
 
 use MonIndemnisationJustice\Entity\Usager;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -16,7 +17,8 @@ class UsagerChecker implements UserCheckerInterface
         }
     }
 
-    public function checkPostAuth(UserInterface $user): void
+    public function checkPostAuth(UserInterface $user, ?TokenInterface $token = null): void
     {
+        // Rien à faire
     }
 }
