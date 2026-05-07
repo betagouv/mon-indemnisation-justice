ce9326811ad291568c52e9898ea70d27ee637140
Pierre LEMEE <pierre@pierrelemee.fr>
2026-05-07 21:42:25 +0200

    Transmettre le contexte utilisateur à Sentry

--- frontend/src/apps/agent/fdo/_init/_router.ts
+++ frontend/src/apps/agent/fdo/_init/_router.ts
@@ -3,6 +3,7 @@ import { container } from "./_container.ts";
 import { createRouter } from "@tanstack/react-router";
 import { AgentManagerInterface } from "@/common/services/agent/agent.ts";
 import { AgentContext } from "@/apps/agent/_commun/contexts";
+import * as Sentry from "@sentry/browser";
 
 // Création du router Tanstack
 
@@ -11,6 +12,12 @@ await container
   .get(AgentManagerInterface.$)
   .moi()
   .then((context: AgentContext) => {
+    Sentry.setTag("app", "fdo");
+    Sentry.setUser({
+      id: context.agent.id,
+      email: context.agent.courriel,
+      username: context.agent.nomComplet(),
+    });
     RouteurFDO = createRouter({
       routeTree,
       defaultPreload: "intent",
