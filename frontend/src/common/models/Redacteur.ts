caba5cfefe9f47ddf6b25c8c0d51dcd6313818ba
Pierre LEMEE <pierre@pierrelemee.fr>
2026-05-21 17:12:40 +0200

    Rétablir la réactivité mobx

--- frontend/src/common/models/Redacteur.ts
+++ frontend/src/common/models/Redacteur.ts
@@ -2,7 +2,7 @@ export class Redacteur {
   public id: number;
   public nom: string;
 
-  public equals(other: Redacteur | null): boolean {
+  public equals(other?: Redacteur): boolean {
     return this.id === other?.id;
   }
 }
