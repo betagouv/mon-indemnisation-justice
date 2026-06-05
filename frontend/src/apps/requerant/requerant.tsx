27ca9259612c866079e40201f3f16dbc26aa4c68
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2026-06-05 10:37:45 +0200

    Afficher un Tourniquet avec un message de chargement au démarrage de l'app

--- frontend/src/apps/requerant/requerant.tsx
+++ frontend/src/apps/requerant/requerant.tsx
@@ -1,5 +1,7 @@
 import "@/apps/_init.ts";
 import { queryClient } from "@/apps/requerant/query";
+import { RouteurRequerant } from "@/apps/requerant/routeur";
+import { Chargement } from "@/common/composants/dsfr/Chargement";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Link, LinkProps, RouterProvider } from "@tanstack/react-router";
 import React, { JSX, StrictMode } from "react";
@@ -8,7 +10,6 @@ import { container } from "@/apps/requerant/container.ts";
 import { Provider } from "inversify-react";
 import ReactDOM from "react-dom/client";
 
-import { RouteurRequerant } from "@/apps/requerant/routeur";
 import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
 import { ColorScheme } from "@codegouvfr/react-dsfr/useIsDark";
 import { Crisp } from "crisp-sdk-web";
@@ -18,8 +19,6 @@ let theme: ColorScheme | "system" = "system";
 try {
   theme = localStorage.getItem("scheme") as ColorScheme;
 } catch (error) {
-  // Firefox: "The operation is insecure"
-  // Chrome: "Failed to read the 'localStorage' property from 'Window'"
   theme = "system";
 }
 
@@ -49,8 +48,12 @@ root.render(
   <StrictMode>
     <CacheBuster
       currentAppVersion={import.meta.env.VITE_MIJ_VERSION || "dev"}
-      // TODO créer un spinner pleine page avec texte https://cssloaders.github.io/
-      loadingComponent={<div>Chargement en cours...</div>}
+      loadingComponent={
+        <Chargement
+          titre="Mon Indemnisation Justice"
+          message="Chargement en cours..."
+        />
+      }
       hideConsoleLogs={import.meta.env.PROD}
       metaJsonPath={"/meta/version.json"}
     >
