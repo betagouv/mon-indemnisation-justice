7189c38dad98b47c34cd6c4d9d04662b85a0f995
Wafa Ben-ayed <wafa.ben-ayed@justice.gouv.fr>
2026-06-10 13:52:39 +0200

    Simplifier le modèle TestEligibilite (espace public)

--- frontend/src/apps/public/models/TestEligibilite.ts
+++ frontend/src/apps/public/models/TestEligibilite.ts
@@ -11,23 +11,22 @@ export class TestEligibilite {
   @DateTransform(true)
   public dateDecision?: Date;
 
-  @Transform(({ value }: { value: string }) => value as ActionContentieuse)
   public actionContentieuse?: ActionContentieuse;
 
-  @Transform(({ value }: { value: unknown }) => (Array.isArray(value) ? value : []) as TypeDecision[])
-  public typeDecision: TypeDecision[] = [];
+  public typeDecision?: TypeDecision;
 
-  @Transform(({ value }: { value: string[] }) => value as PieceProcedure[])
+  @Transform(({ value }: { value: unknown }) => (Array.isArray(value) ? value : []) as PieceProcedure[])
   public piecesProc: PieceProcedure[] = [];
 
-  @Transform(({ value }: { value: unknown }) => value === true || value === "oui")
+  @Transform(({ value }: { value: boolean | string }) => value === true || value === "oui")
   public preuvesDiligences?: boolean;
 
   get estEligible(): boolean {
     return (
       calculerPrescription(this.dateDecision).rempli &&
       this.actionContentieuse === ActionContentieuse.Non &&
-      this.typeDecision.length > 0 &&
+      this.typeDecision !== undefined &&
+      this.typeDecision !== TypeDecision.Aucune &&
       this.piecesProc.length > 0 &&
       this.preuvesDiligences === true
     );
